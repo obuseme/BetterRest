4a0d5185a41039869a872574e750fb33ffcce8cf
obuseme <obusek@gmail.com>
2020-07-28 07:50:28 -0400

    Day 26

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -8,8 +8,15 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var sleepAmount = 8.0
+    @State private var wakeUp = Date()
     var body: some View {
-        Text("Hello, world!").padding()
+        Form {
+            Stepper(value: $sleepAmount, in: 4...12, step: 0.25) {
+                Text("\(sleepAmount, specifier: "%g") hours")
+            }
+            DatePicker("Please enter a date", selection: $wakeUp, displayedComponents: .hourAndMinute).labelsHidden()
+        }
     }
 }
 
