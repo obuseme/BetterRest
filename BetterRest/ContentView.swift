bd3fabd94c43a1ed15057d27b8283082fefff099
obuseme <obusek@gmail.com>
2020-07-28 07:21:18 -0400

    Initial Commit

---
+++ BetterRest/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  BetterRest
+//
+//  Created by Andrew Obusek on 7/28/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
